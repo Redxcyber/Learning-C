5a2b6c67d4793dbf79d2344f1778877170bf9d26
Redxcyber <mrdark9628@gmail.com>
2024-09-24 16:54:45 +0530

    add file to iterative statements

--- 06-iterative-statements/41-armstrongNumber.c
+++ 06-iterative-statements/41-armstrongNumber.c
@@ -1,59 +1,42 @@
 //check whether a number is armstong number or not 
-//if number = sum of cube of its digit then, it is armstrong else not
+//if number = sum of power(no of digits) of its digit then, it is armstrong else not
 
 #include<stdio.h>
-int main(){
-    // int num;
-    // int sumOfCubeOfDigit=0;
-    // printf("Enter num : ");
-    // scanf("%d",&num);
-
-    // if(num<0){
-    //     printf("Enter positive number\n");
-    //     return 1;
-    // }
-
-    // for(int i=num; i>0; i/=10){
-    //     int digit = i%10;
-    //     int cubeOfDigit = digit * digit * digit;
-    //     sumOfCubeOfDigit += cubeOfDigit;
-    // }
-
-    // if(sumOfCubeOfDigit==num){
-    //     printf("Yes, %d is armstrong number",num);
-    // }else{
-    //     printf("No, %d is not armstrong number",num);
-    // }
-    
-    // return 0;
-
-
-    while(1){
-
-        int num;
-        int sumOfCubeOfDigit=0;
-        printf("Enter num : ");
-        scanf("%d",&num);
-
-        if(num>=0){
-            for(int i=num; i>0; i/=10){
-                int digit = i%10;
-                int cubeOfDigit = digit * digit * digit;
-                sumOfCubeOfDigit += cubeOfDigit;
-            }
-
-            if(sumOfCubeOfDigit==num){
-                printf("Yes, %d is armstrong number\n\n",num);
-            }else{
-                printf("No, %d is not armstrong number\n\n",num);
-            }
-
-        }else{
-            printf("WARNING : Enter positive number\n\n");
-            continue;
-        }
-
+#include<math.h>
+int power(int base, int exp){
+    int result = 1;
+    for(int i=1; i<=exp; i++){
+        result *= base;
     }
+    return result;
+}
+int main(){
+    int num;
+    int digitCount=0, sumOfPowerOfDigit=0;
 
+    printf("Enter positive number : ");
+    scanf("%d",&num);
+
+    if(num<0){
+        printf("Negative numbers are not allowed ");
+
+    }else{
+        for(int i=num; i>0; i/=10){
+            digitCount++;
+        }
+        for(int i=num; i>0; i/=10){
+            int digit = i%10;
+            int powerOfDigit = power(digit, digitCount);
+            sumOfPowerOfDigit += powerOfDigit;
+            printf("powerOfDigit = %d\n",powerOfDigit);
+        }
+        printf("sumOfPowerOfDigit = %d\n",sumOfPowerOfDigit);
+        printf("num = %d\n",num);
+        if(sumOfPowerOfDigit==num){
+            printf("Yes, %d is armstrong number\n\n",num);
+        }else{
+            printf("No, %d is not armstrong number\n\n",num);
+        }
+    }
     return 0;
 }
