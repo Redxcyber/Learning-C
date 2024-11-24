13d967e685e9250cc0d6e064d49a289364093cac
Redxcyber <mrdark9628@gmail.com>
2024-11-24 22:26:48 +0530

    add file to iterative statements

--- 06-iterative-statements/46-fibonacciSeries.c
+++ 06-iterative-statements/46-fibonacciSeries.c
@@ -16,5 +16,21 @@ int main(){
         }
         printf("%d ",c);
     }
+
+    //method-02
+    int a=0, b=1, c;
+    for(int i=1; i<=n; i++){
+        if(i==1){
+            c=a;
+        }else if(i==2){
+            c=b;
+        }else{
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        printf("%d ",c);
+    }
+    
     return 0;
 }
